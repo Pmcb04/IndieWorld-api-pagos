aed1cdf612d7d49da2b2bc0090fcc7c55fb3b275
Pedro Miguel Carmona Broncano <63114650+Pmcb04@users.noreply.github.com>
2023-12-15 08:21:46 +0000

    add swagger-stats

--- index.js
+++ index.js
@@ -17,10 +17,29 @@ var options = {
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
 
+
+
 const metricsInterval = Prometheus.collectDefaultMetrics()
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+});
+
+
+const express = require('express');
+const swStats = require('swagger-stats');
+const apiSpec = require('./swagger.json');
+
+const app2 = express();
+
+// Configuración de Swagger Stats
+app2.use(swStats.getMiddleware({ swaggerSpec: apiSpec }));
+
+
+// Iniciar el servidor
+const PORT = 4444;
+app2.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
